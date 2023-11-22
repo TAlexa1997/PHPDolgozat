60343d2ee89a858adc0dd4093e76b10ad6d3c028
Tóth Alexandra <alejandra.marianella.8@gmail.com>
2023-11-22 19:49:51 +0100

    first commit

---
+++ Esemeny/app/Http/Controllers/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = response()->json(Product::all()); 
+        return $products; 
+    } 
+ 
+    public function show($id){ 
+        $product = response()->json(Product::findOrFail($id)); 
+        return $product; 
+    } 
+ 
+    public function store(Request $request){ 
+        $product = new Product(); 
+        $product->item_id = $request-> item_id; 
+        $product->date = $request->date; 
+        $product->save(); 
+    } 
+ 
+    public function update(Request $request, $id){ 
+        $product = Product::findOrFail($id); 
+        $product->item_id = $request-> item_id; 
+        $product->date = $request->date; 
+        $product->save(); 
+    } 
+}
---
+++ Esemeny/database/factories/ProductTypeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product_type>
+ */
+class ProductTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->realText(50),
+            'cost' => rand(20,30),
+        ];
+    }
+}
